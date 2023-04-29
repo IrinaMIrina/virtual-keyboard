8503d3c8635dfa65a861368da3aedb362fe75e2c
Iryna M <iirinaaryna@gmail.com>
2023-04-29 17:26:38 +0300

    feat: add main descriptions via js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,32 @@
+const div = document.createElement('div');
+div.className = "wrapper";
+div.classList.add("wrapper");
+document.body.append(div);
 
+const p = document.createElement('p');
+p.className = "title";
+p.classList.add("title");
+p.textContent = "RSS Виртуальная клавиатура";
+document.querySelector(".wrapper").append(p);
+
+const textarea = document.createElement('textarea');
+textarea.className = "body--textarea textarea";
+textarea.classList.add("title");
+document.querySelector(".wrapper").append(textarea);
+
+const div2 = document.createElement('div');
+div2.className = "body--keyboard keyboard";
+div2.classList.add("keyboard");
+document.querySelector(".wrapper").append(div2);
+
+const description = document.createElement('p');
+description.className = "description";
+description.classList.add("description");
+description.textContent = "Клавиатура создана в операционной системе Windows";
+document.querySelector(".wrapper").append(description);
+
+const language = document.createElement('p');
+language.className = "description";
+language.classList.add("description");
+language.textContent = "Для переключения языка комбинация: левыe ctrl + alt";
+document.querySelector(".wrapper").append(language);
