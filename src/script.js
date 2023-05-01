0487c0c340af9ba4fc11f4b19a1815aa6884b6a2
Iryna M <iirinaaryna@gmail.com>
2023-05-01 21:04:55 +0300

    feat: complete rows with the keys

--- src/script.js
+++ src/script.js
@@ -20,27 +20,27 @@ div2.classList.add("keyboard");
 document.querySelector(".wrapper").append(div2);
 
 const row1 = document.createElement('div');
-row1.className = "row";
+row1.className = "row1";
 row1.classList.add("row");
 document.querySelector(".keyboard").append(row1);
 
 const row2 = document.createElement('div');
-row2.className = "row";
+row2.className = "row2";
 row2.classList.add("row");
 document.querySelector(".keyboard").append(row2);
 
 const row3 = document.createElement('div');
-row3.className = "row";
+row3.className = "row3";
 row3.classList.add("row");
 document.querySelector(".keyboard").append(row3);
 
 const row4 = document.createElement('div');
-row4.className = "row";
+row4.className = "row4";
 row4.classList.add("row");
 document.querySelector(".keyboard").append(row4);
 
 const row5 = document.createElement('div');
-row5.className = "row";
+row5.className = "row5";
 row5.classList.add("row");
 document.querySelector(".keyboard").append(row5);
 
@@ -55,3 +55,56 @@ language.className = "description";
 language.classList.add("description");
 language.textContent = "Для переключения языка комбинация: левыe ctrl + alt";
 document.querySelector(".wrapper").append(language);
+
+const rowFirst = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8];
+function init() {
+  let result = '';
+  for (let i = 0; i < rowFirst.length; i += 1) {
+    if (i === 13) {
+      result += '<div class = "backspace"></div>';
+    }
+    result += `<div class="key" >${String.fromCharCode(rowFirst[i])}</div>`;
+  }
+  document.querySelector('.row1').innerHTML = result;
+}
+init();
+
+const rowSecond = [9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220];
+function initSecondRow() {
+  let result = '';
+  for (let i = 0; i < rowSecond.length; i += 1) {
+    result += `<div class="key" >${String.fromCharCode(rowSecond[i])}</div>`;
+  }
+  document.querySelector('.row2').innerHTML = result;
+}
+initSecondRow();
+
+const rowThird = [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13];
+function initThirdRow() {
+  let result = '';
+  for (let i = 0; i < rowThird.length; i += 1) {
+    result += `<div class="key" >${String.fromCharCode(rowThird[i])}</div>`;
+  }
+  document.querySelector('.row3').innerHTML = result;
+}
+initThirdRow();
+
+const rowFourth = [16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 16, 35];
+function initFourthRow() {
+  let result = '';
+  for (let i = 0; i < rowFourth.length; i += 1) {
+    result += `<div class="key" >${String.fromCharCode(rowFourth[i])}</div>`;
+  }
+  document.querySelector('.row4').innerHTML = result;
+}
+initFourthRow();
+
+const rowFifth = [17, 91, 18, 32, 18, 93, 17, 37, 38, 40, 39];
+function initFifthRow() {
+  let result = '';
+  for (let i = 0; i < rowFifth.length; i += 1) {
+    result += `<div class="key" >${String.fromCharCode(rowFifth[i])}</div>`;
+  }
+  document.querySelector('.row5').innerHTML = result;
+}
+initFifthRow();
